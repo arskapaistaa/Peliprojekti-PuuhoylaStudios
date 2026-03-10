9c787ae8b08b961e1786fad24d48df0d7ff3ca7a
Joonas Björninen <joonas.bjorninen1@gmail.com>
2026-03-10 11:29:00 +0200

    Exit logic

---
+++ Hiirilabyrintti/Scripts/Exit.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+public partial class Exit : Area2D
+{
+
+	public override void _Ready()
+	{
+		BodyEntered += OnBodyEntered;
+	}
+
+	// Simple method to exit labyrinth when player enters area2D
+	private void OnBodyEntered(Node2D body)
+	{
+		// Load end game scene when enter area2D
+		if (body is CharacterBody2D)
+		{
+
+			GetTree().ChangeSceneToFile("res://Scenes/Game Scenes/end_game_scene.tscn");
+		}
+	}
+}
