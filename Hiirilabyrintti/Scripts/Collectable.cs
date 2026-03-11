abacb5c89b82b0c8d43d9a0ed4328cf1a9f863c3
Joonas Björninen <joonas.bjorninen1@gmail.com>
2026-03-11 12:31:46 +0200

    Pistelaskulogiikka

---
+++ Hiirilabyrintti/Scripts/Collectable.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Formats.Asn1;
+using Godot;
+
+public partial class Collectable : Area2D
+{
+	private bool _isCollected = false;
+
+	public bool IsCollected
+	{
+		get { return _isCollected; }
+	}
+
+	public override void _EnterTree()
+	{
+		BodyEntered += OnBodyEntered;
+	}
+	public override void _ExitTree()
+	{
+		BodyEntered -= OnBodyEntered;
+	}
+
+	private void OnBodyEntered(Node2D body)
+	{
+		if (body is Mouse mouse)
+		{
+			_isCollected = true;
+			Collect(mouse);
+
+			QueueFree();
+		}
+	}
+
+	protected virtual void Collect(Mouse mouse)
+	{
+
+	}
+}
---
+++ Hiirilabyrintti/Scripts/GameManager.cs
@@ -0,0 +1,83 @@
+using Godot;
+using System;
+
+public partial class GameManager : Node
+{
+
+	public static GameManager Instance
+	{
+		get;
+		private set;
+	}
+
+	public GameManager()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else if (Instance != this)
+		{
+			// Jos singleton-olio on jo olemassa, tuhotaan luotu olio.
+			QueueFree();
+			return;
+		}
+	}
+
+	// Cheese part
+	private int _cheeseScore = 0;
+
+	public int CheeseScore
+	{
+		get {return _cheeseScore; }
+		set
+		{
+			// Min and Max cheese score
+			_cheeseScore = Mathf.Clamp(value, 0, 999);
+			// For Debugging
+			GD.Print("Cheese Score:" + _cheeseScore);
+		}
+	}
+
+	// Drill part
+	private int _drillScore = 0;
+	public int DrillScore
+	{
+		get {return _drillScore; }
+		set
+		{
+			// Min and Max drill score
+			_cheeseScore = Mathf.Clamp(value, 0, 999);
+			// For Debugging
+			GD.Print("Drill Score:" + _drillScore);
+
+			var cheeseCounter = GetNode("CheeseCounter");
+			cheeseCounter.Call("UpdateScore");
+		}
+	}
+
+	// Method to call from cheese class
+	public bool AddCheese(int amount)
+	{
+		if (amount < 0)
+		{
+			return false;
+		}
+
+		CheeseScore += amount;
+		return true;
+	}
+
+	// Method to call from drill class
+	public bool AddDrill(int amount)
+	{
+		if (amount < 0)
+		{
+			return false;
+		}
+
+		DrillScore += amount;
+		return true;
+	}
+
+}
