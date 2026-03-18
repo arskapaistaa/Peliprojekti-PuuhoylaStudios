79eda8cda648da13ead07fac31512d8615aacd87
Joonas Björninen <joonas.bjorninen1@gmail.com>
2026-03-18 15:14:01 +0200

    Yes, lot of changes.

--- Hiirilabyrintti/Scripts/Cheese.cs
+++ Hiirilabyrintti/Scripts/Cheese.cs
@@ -4,10 +4,14 @@ using System;
 public partial class Cheese : Collectable
 {
 	private int _cheeseScore = 1;
+	[Export] private CanvasLayer _canvasLayer;
 
 	protected override void Collect(Mouse mouse)
 	{
 		GD.Print("Cheese collected");
 		GameManager.Instance.AddCheese(_cheeseScore);
+
+		var label = GetNode<Label>("/root/Main/CanvasLayer/CheeseLabel");
+		// TODO loppuun!
 	}
 }
--- Hiirilabyrintti/Scripts/FinalGameScene.cs
+++ Hiirilabyrintti/Scripts/FinalGameScene.cs
@@ -3,10 +3,37 @@ using System;
 
 public partial class FinalGameScene : Control
 {
-	[Export] private string[] _questionResult;
+	[Export] private Label _firstDialog;
+
+	// Arrays are for lanquages.
+	[Export] private String[] _firstDialogArray = {};
+	[Export] private String[] _edistaja = {};
+	[Export] private String[] _etenija = {};
+	[Export] private String[] _etsija = {};
+
+	[Export] private ColorRect _edistajaBar;
+	[Export] private ColorRect _etenijaBar;
+	[Export] private ColorRect _estijaBar;
+	[Export] private Label _edistajaLabel;
+	[Export] private Label _etenijaLabel;
+	[Export] private Label _etsijaLabel;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// Set First dialog Fin or Eng
+		_firstDialog.Text = _firstDialogArray[SettingsManager.Instance.Language];
+
+		// Set color rect sizes to Score * 4. Beacause max width is 400px, and we have currently 10 questions.
+
+		_edistajaBar.SetSize(new Vector2(GameManager.Instance.EdistajaScore * 4, 20));
+		_etenijaBar.SetSize(new Vector2(GameManager.Instance.EtenijaScore * 4, 20));
+		_estijaBar.SetSize(new Vector2(GameManager.Instance.EtsijaScore * 4, 20));
+
+		// Set label texts, add Score and %.
+		_edistajaLabel.Text = _edistaja[SettingsManager.Instance.Language] + " " + GameManager.Instance.EdistajaScore + "%";
+		_etenijaLabel.Text = _etenija[SettingsManager.Instance.Language] + " " + GameManager.Instance.EtenijaScore + "%";
+		_etsijaLabel.Text = _etsija[SettingsManager.Instance.Language] + " " + GameManager.Instance.EtsijaScore + "%";
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
