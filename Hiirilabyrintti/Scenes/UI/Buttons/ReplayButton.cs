ed99aae8ae9f5265ec6d9b654ba5869081aac238
Joonas Björninen <joonas.bjorninen1@gmail.com>
2026-03-30 12:33:17 +0300

    Fix: Set values back to 0
    When replaying.

--- Hiirilabyrintti/Scenes/UI/Buttons/ReplayButton.cs
+++ Hiirilabyrintti/Scenes/UI/Buttons/ReplayButton.cs
@@ -13,5 +13,14 @@ public partial class ReplayButton : Button
 	public void ButtonPressed()
 	{
 		GetTree().CallDeferred("change_scene_to_file", "res://Scenes/Game Scenes/MainScene.tscn");
+
+		// Reset everything
+		GameManager.Instance.CheeseScore = 0;
+		GameManager.Instance.DrillScore = 0;
+		GameManager.Instance.EtsijaScore = 0;
+		GameManager.Instance.EtenijaScore = 0;
+		GameManager.Instance.EdistajaScore = 0;
+
+		// TODO: Load different scene
 	}
 }
