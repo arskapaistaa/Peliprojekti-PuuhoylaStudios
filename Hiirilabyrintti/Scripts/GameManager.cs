2cfddc0307783ca06c7af541b7b98acdf6e89c81
Arvo <arvokoskinen@hotmail.com>
2026-04-14 22:20:00 +0300

    vika committi tähän hätään. tsekataan loppuun yhessä

--- Hiirilabyrintti/Scripts/GameManager.cs
+++ Hiirilabyrintti/Scripts/GameManager.cs
@@ -95,7 +95,7 @@ public partial class GameManager : Node
 
 	// Drill part
 	private int _drillScore = 0;
-	public int _neededToBuildDrill = 1;
+	[Export] public int _neededToBuildDrill = 1;
 	public bool _hasDrill = false;
 
 	public int DrillScore
