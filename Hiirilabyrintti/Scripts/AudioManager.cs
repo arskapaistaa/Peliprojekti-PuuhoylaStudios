62905721991bb9ba7d2ee090f152b1f14f4b2b71
Joonas Björninen <joonas.bjorninen1@gmail.com>
2026-04-17 10:49:24 +0300

    Add: Music

--- Hiirilabyrintti/Scripts/AudioManager.cs
+++ Hiirilabyrintti/Scripts/AudioManager.cs
@@ -4,12 +4,18 @@ using System;
 public partial class AudioManager : Node
 {
 	[Export] private AudioStreamPlayer2D _waterDropsSFX = null;
+	[Export] private AudioStreamPlayer _music = null;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		if (SettingsManager.Instance.Volume)
 		{
-		_waterDropsSFX.Play();
+			_waterDropsSFX.Play();
+		}
+
+		if (SettingsManager.Instance.Music)
+		{
+			_music.Play();
 		}
 	}
 
