0708628f2bbfd9b9090d366e394d5ef568e3d520
Joonas Björninen <joonas.bjorninen1@gmail.com>
2026-04-21 19:38:26 +0300

    Mouse trap particles

--- Hiirilabyrintti/Scripts/Mousetrap.cs
+++ Hiirilabyrintti/Scripts/Mousetrap.cs
@@ -4,19 +4,14 @@ using System.Collections;
 
 public partial class Mousetrap : Area2D
 {
-
 	[Export] private AnimatedSprite2D _mouseTrap;
 	[Export] private AudioStreamPlayer2D _mouseTrapSFX = null;
+	[Export] private GpuParticles2D _cheese = null;
 	bool _isActive = true;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		BodyEntered += OnBodyEntered;
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
 	}
 
 	private void OnBodyEntered(Node2D body)
@@ -26,14 +21,30 @@ public partial class Mousetrap : Area2D
 		if (body is Mouse && _isActive)
 		{
 			GD.Print("Mouse detected!");
-			GameManager.Instance.RemoveCheese(3);
+
 			_mouseTrap.Play("default");
 			_isActive = false;
 
+			// Sound
 			if (SettingsManager.Instance.Volume)
 			{
 				_mouseTrapSFX.Play();
 			}
+
+			// Particles
+			if (GameManager.Instance.CheeseScore >= 3)
+			{
+				_cheese.Amount = 3;
+				_cheese.Emitting = true;
+			}
+			else if (GameManager.Instance.CheeseScore < 3)
+			{
+				_cheese.Amount = GameManager.Instance.CheeseScore;
+				_cheese.Emitting = true;
+			}
+
+			GameManager.Instance.RemoveCheese(3);
+
 		}
 	}
 
